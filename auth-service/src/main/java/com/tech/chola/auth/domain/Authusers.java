7fa693171b2813fd94fdd66511d0b5b3d9eec845
suran2003in <suran2003in@gmail.com>
2018-02-04 23:47:50 +0530

    implement core userdetails

--- auth-service/src/main/java/com/tech/chola/auth/domain/Authusers.java
+++ auth-service/src/main/java/com/tech/chola/auth/domain/Authusers.java
@@ -3,11 +3,15 @@
  */
 package com.tech.chola.auth.domain;
 
-import javax.annotation.Generated;
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 
@@ -18,7 +22,11 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="userauth")
-public class Authusers {
+public class Authusers  implements UserDetails{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@Column(name ="username")
 	private String username;
@@ -57,4 +65,44 @@ public class Authusers {
 	                "Users [id=%d, userName='%s', password='%s']",
 	                username, password, null);
 	    }
+
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
